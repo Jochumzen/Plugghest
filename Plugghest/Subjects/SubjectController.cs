d446ce851df7415fc07595a04c66a32185d828ff
Jochumzen <peter.jochumzen@nek.lu.se>
2014-04-17 15:44:16 +0530

    Update Subject on Plugghest Library

--- Plugghest/Subjects/SubjectController.cs
+++ Plugghest/Subjects/SubjectController.cs
@@ -19,42 +19,6 @@ namespace Plugghest.Subjects
     public class SubjectController
     {
 
-        public void DeleteItem(int itemId, int moduleId)
-        {
-            var t = GetItem(itemId, moduleId);
-            DeleteItem(t);
-        }
-
-        public void DeleteItem(SubjectItem t)
-        {
-            using (IDataContext ctx = DataContext.Instance())
-            {
-                var rep = ctx.GetRepository<SubjectItem>();
-                rep.Delete(t);
-            }
-        }
-
-        public IEnumerable<SubjectItem> GetItems(int moduleId)
-        {
-            IEnumerable<SubjectItem> t;
-            using (IDataContext ctx = DataContext.Instance())
-            {
-                var rep = ctx.GetRepository<SubjectItem>();
-                t = rep.Get(moduleId);
-            }
-            return t;
-        }
-
-        public SubjectItem GetItem(int itemId, int moduleId)
-        {
-            SubjectItem t;
-            using (IDataContext ctx = DataContext.Instance())
-            {
-                var rep = ctx.GetRepository<SubjectItem>();
-                t = rep.GetById(itemId, moduleId);
-            }
-            return t;
-        }
 
         public void UpdateItem(SubjectItem t)
         {
@@ -65,42 +29,21 @@ namespace Plugghest.Subjects
             }
         }
 
-        public DataTable GetNodeItems()
+
+        public List<Subject_Tree> GetSubject_Item()
         {
-            DataTable dt = new DataTable();
-            dt.Columns.Add("NodeID");
-            dt.Columns.Add("NodeTitle");
-            dt.Columns.Add("Mother");
-            dt.Columns.Add("Order");
-            IEnumerable<SubjectItem> t;
+            List<Subject_Tree> objsubjectitem = new List<Subject_Tree>();
             using (IDataContext ctx = DataContext.Instance())
             {
-                var rep = ctx.GetRepository<SubjectItem>();
-                t = rep.Get();
-                foreach (SubjectItem val in t)
-                {
-                    dt.Rows.Add(val.SubjectID, val.Subject, val.Mother, val.Order);
-                }
-            }
-
-            return dt;
-        }
-
-        public List<Subject_Item> GetSubject_Item()
-        {
-            List<Subject_Item> objsubjectitem = new List<Subject_Item>();
-            using (IDataContext ctx = DataContext.Instance())
-            {
-                var rec = ctx.ExecuteQuery<Subject_Item>(CommandType.TableDirect, "select * from SubjectItems order by [Order]");
+                var rec = ctx.ExecuteQuery<Subject_Tree>(CommandType.TableDirect, "select * from SubjectItems order by [Order]");
                 foreach (var val in rec)
                 {
-                    objsubjectitem.Add(new Subject_Item { SubjectID = val.SubjectID, Subject = val.Subject, label = val.Subject, Mother = val.Mother, Order = val.Order });
+                    objsubjectitem.Add(new Subject_Tree { SubjectID = val.SubjectID, Subject = val.Subject, label = val.Subject, Mother = val.Mother, Order = val.Order });
                 }
-
             }
-
             return objsubjectitem;
         }
+
 
         //insert on subject
         public void CreateSubject(SubjectItem t)
@@ -135,7 +78,7 @@ namespace Plugghest.Subjects
                 var rec = ctx.ExecuteQuery<SubjectItem>(CommandType.TableDirect, "select * from SubjectItems where Mother=" + MotherName + "AND [ORDER] >=" + order + " order by [order]");
                 foreach (var val in rec)
                 {
-                    sublist.Add(new SubjectItem { SubjectID = val.SubjectID, Subject = val.Subject, Mother = val.Mother });
+                    sublist.Add(new SubjectItem(val.SubjectID,val.Subject,val.Mother,val.Order));
                 }
             }
             return sublist;
