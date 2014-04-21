11a8e41c570d9edcd740b2e40b4d8c0d04f0b802
Jochumzen <peter.jochumzen@nek.lu.se>
2014-04-21 21:55:43 +0530

    changes

---
+++ EditCourseItems/EditCourseItems/View.ascx
@@ -0,0 +1,60 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Christoc.Modules.EditCourseItems.View" %>
+
+<div></div>
+<link href="/DesktopModules/Plugghest_Subjects/js/jqtree.css" rel="stylesheet" />
+<script src="/DesktopModules/Plugghest_Subjects/js/tree.jquery.js"></script>
+
+<div>
+    <asp:Label ID="lblHeading" runat="server">Heading</asp:Label>
+    <asp:TextBox ID="txtheading" runat="server"></asp:TextBox>
+    <asp:Button ID="btnAddHeading" runat="server" Text="Insert Heading"/>
+</div>
+<div>
+    <asp:Label ID="lbladdPlug" runat="server">Add Plug</asp:Label>
+    <asp:TextBox ID="txtaddPlug" runat="server"></asp:TextBox>
+    <asp:Button ID="btnAddplug" runat="server" Text="Insert Plug" />
+</div>
+<div>
+    <asp:Button  ID="btnRemoveSelectedItem" Text="Remove Selected item" runat="server"/>
+</div>
+<div>
+    <asp:Button ID="btnSave" runat="server" Text="Save" />
+    <asp:button ID="btnCancel" runat="server" Text="Cancel" />
+</div>
+  <div class="resp_container">
+          <div class="tree">
+       <div id="tree2"></div>
+      </div>
+      <asp:HiddenField runat="server" ID="hdnTreeData" />
+      </div>
+<asp:Label runat="server" ID="lblError" ForeColor="Red"></asp:Label>
+<asp:HiddenField ID="hdnNodeSubjectId" runat="server" /> 
+
+<script type="text/javascript">
+
+    $(document).ready(function () {
+        $('#tree2').tree({
+            data: eval($("#" + '<%=hdnTreeData.ClientID%>').attr('value')),
+            selectable: true,
+            autoEscape: false,
+            autoOpen: true
+        });
+    });
+
+    function getCourseid()
+    {
+        var node = $('#tree2').tree('getSelectedNode');
+
+        var Error = "";
+
+        if (!node)
+            Error = 'Please Select Node \n';
+
+        if (Error != "") {
+            alert(Error);
+            return false;
+        }
+        $("#<%=hdnNodeSubjectId.ClientID%>").val(node.SubjectID);
+    }
+
+</script>
---
+++ EditCourseItems/EditCourseItems/View.ascx.cs
@@ -0,0 +1,127 @@
+﻿/*
+' Copyright (c) 2014  Christoc.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using System.Web.UI.WebControls;
+using Christoc.Modules.EditCourseItems.Components;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Services.Localization;
+using DotNetNuke.UI.Utilities;
+using Plugghest.Subjects;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using Plugghest.Courses;
+
+namespace Christoc.Modules.EditCourseItems
+{
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// The View class displays the content
+    /// 
+    /// Typically your view control would be used to display content or functionality in your module.
+    /// 
+    /// View may be the only control you have in your project depending on the complexity of your module
+    /// 
+    /// Because the control inherits from EditCourseItemsModuleBase you have access to any custom properties
+    /// defined there, as well as properties from DNN such as PortalId, ModuleId, TabId, UserId and many more.
+    /// 
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+    public partial class View : EditCourseItemsModuleBase, IActionable
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+            try
+            {
+                BindTree();
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+
+        public void BindTree()
+        {
+            CourseHandler objcoursehdler = new CourseHandler();
+            var Courselist = objcoursehdler.GetCourseItems(25);
+
+            var tree = BuildTree(Courselist);
+
+            JavaScriptSerializer TheSerializer = new JavaScriptSerializer();
+
+            hdnTreeData.Value = TheSerializer.Serialize(tree);
+        }
+
+
+        #region Create Tree
+
+        //Recursive function for create tree....
+        public IList<Course_Tree> BuildTree(IEnumerable<Course_Tree> source)
+        {
+            var groups = source.GroupBy(i => i.Mother);
+
+
+            var roots = groups.FirstOrDefault(g => g.Key.HasValue == false).ToList();
+
+            if (roots.Count > 0)
+            {
+                var dict = groups.Where(g => g.Key.HasValue).ToDictionary(g => g.Key.Value, g => g.ToList());
+                for (int i = 0; i < roots.Count; i++)
+                    AddChildren(roots[i], dict);
+            }
+
+            return roots;
+        }
+
+        //To Add Child
+        private void AddChildren(Course_Tree node, IDictionary<int, List<Course_Tree>> source)
+        {
+            if (source.ContainsKey(node.CourseId))
+            {
+                node.children = source[node.CourseId];
+                for (int i = 0; i < node.children.Count; i++)
+                    AddChildren(node.children[i], source);
+            }
+            else
+            {
+                node.children = new List<Course_Tree>();
+            }
+        }
+
+        #endregion
+
+
+
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                    {
+                        {
+                            GetNextActionID(), Localization.GetString("EditModule", LocalResourceFile), "", "", "",
+                            EditUrl(), false, SecurityAccessLevel.Edit, true, false
+                        }
+                    };
+                return actions;
+            }
+        }
+    }
+}
